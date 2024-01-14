65062f550888f2c4a0842889b86058837f8dddd9
j3ffhu <j3ffhu@gmail.com>
2024-01-14 14:51:07 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ -z "${MOCK}" ]]; then
+if [ ! -z "${MOCK}" ]; then
   nohup java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.Server -s  2>&1 &
   nohup java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.Server 2>&1 &
 fi
