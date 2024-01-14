a98b8f6d6c985589b15f90c8655fbfe95401a9b5
j3ffhu <j3ffhu@gmail.com>
2024-01-14 12:55:28 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ nohup bash -c "java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.Se
 nohup bash -c "java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.Server 2>&1 &" && sleep 1
 
 # submit host  proj1.3700.network
-java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.SocketClient  -p 27993 localhost alex
-java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.SocketClient  -p 27994 -s localhost alex
+java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.SocketClient  $ARGS
+java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.SocketClient  $ARGS
 
+ 
