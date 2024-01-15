7de970ba087bfe43595c3fe9b05103dd7f0651a6
j3ffhu <j3ffhu@gmail.com>
2024-01-15 00:50:00 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-if [ ! -z "${MOCK}" ]; then
-  nohup java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.Server -s  2>&1 &
-  nohup java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.Server 2>&1 &
-fi
-
-
 # submit host  proj1.3700.network
 java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.SocketClient  $ARGS
 java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.SocketClient  $ARGS_TLS
