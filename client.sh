928252afaf5afe6adc394fababf4cef4245dfb31
j3ffhu <j3ffhu@gmail.com>
2024-01-17 19:10:23 -0800

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-# submit host  proj1.3700.network
-java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.SocketClient  "$@"
-
-# java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.SocketClient  $ARGS
+java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.SocketClient  $ARGS
 
 #  -p 27993 proj1.3700.network john_dos
 # docker build -t cs3700:hw1 . 
