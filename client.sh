fbe13d89488cff19ab71af1b1775059cdd5034c8
j3ffhu <j3ffhu@gmail.com>
2024-01-17 18:27:11 -0800

    Update client.sh

--- client.sh
+++ client.sh
@@ -7,5 +7,6 @@ java -cp ./cs3700-0.0.1-SNAPSHOT-jar-with-dependencies.jar hw1.SocketClient  "$@
 
 #  -p 27993 proj1.3700.network john_dos
 # docker build -t cs3700:hw1 . 
-# docker run -it --env ARGS="-p 27993 proj1.3700.network jane_dos"  --env ARGS_TLS="-p 27994 -s proj1.3700.network jane_dos"  --rm cs3700:hw1
+# docker run -it --env ARGS="-p 27993 proj1.3700.network jane_dos"  --rm cs3700:hw1
+# docker run -it --env ARGS="-p 27994 -s proj1.3700.network jane_dos"  --rm cs3700:hw1
  
